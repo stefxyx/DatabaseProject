88a81275caaea0215cae2d4708c180d0ff85e8ca
zyyswallow <yanyanzhou@dl-wireless-pittnet-150-212-45-149.wireless.pitt.edu>
2014-11-17 17:21:42 -0500

    1111

--- src/java/Handler/EmployeeHandler.java
+++ src/java/Handler/EmployeeHandler.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package Handler;
+import Bean.Employee;
+import Database.Handler;
+import java.util.List;
 
 /**
  *
  * @author Wu
  */
 public class EmployeeHandler {
+    private static EmployeeHandler employeeHandler = new EmployeeHandler();
+    private EmployeeHandler(){
+        
+    }
     
+    public static EmployeeHandler getEmployeeHandler(){
+        return employeeHandler;
+    }
+    
+    public List<Employee> GetEmployeeByID(String [] array){
+        String sql = "sejlr = ?";
+        List<Employee> l = Handler.getCustomerHandler().searchQuery(sql, array, Employee.class);
+        int ret = Handler.getCustomerHandler().updateQuery(sql, array);
+        Handler.getCustomerHandler().insertQuery(sql, array);
+        return l;
+    }
 }
