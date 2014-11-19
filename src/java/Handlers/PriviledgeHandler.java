9dd89ca6806997d907287f5311644c9201ca4ddb
stefxyx <yingxinxie@yingxinxies-MacBook-Pro.local>
2014-11-18 19:48:26 -0500

    3

--- src/java/Handlers/PriviledgeHandler.java
+++ src/java/Handlers/PriviledgeHandler.java
@@ -5,20 +5,72 @@
  */
 package Handlers;
 
+import Bean.Priviledge;
+import Database.Handler;
+import java.util.List;
+
 /**
  *
  * @author Wu
  */
 public class PriviledgeHandler {
-    public static PriviledgeHandler ph = new PriviledgeHandler();
+    public static PriviledgeHandler priviledgeHandler = new PriviledgeHandler();
     
     private PriviledgeHandler(){
         
     }
     
     public static PriviledgeHandler getPrivilegeHandler(){
-        return ph;
+        return priviledgeHandler;
     }
     
+    /**
+     * get Priviledge only by region_id
+     * @param id
+     * @return Priviledge
+     */
+    public Priviledge getPriviledgeById(String id) {
+        String sql = "select * from Priviledge where priviledge_id = ?";
+        String[] para = {id};
+        List<Priviledge> list = Handler.getHandler().searchQuery(sql, para, Priviledge.class);
+        if (list != null) {
+            return list.get(0);
+        } else {
+            return null;
+        }
+    }
     
+    /**
+     * update Priviledge by a given id
+     * @param para
+     * @return 
+     */
+    public int updatePriviledgeById(String [] para) {
+        String sql = "update Priviledge set description = ? where priviledge_id = ?";
+        int ret = Handler.getHandler().updateQuery(sql, para);
+        return ret;
+    }
+    
+    /**
+     * add new Priviledge
+     * @param para
+     * @return int
+     */
+    public int addPriviledge(String [] para){
+        String sql = "intert into Priviledge (description) values (?)";
+        int ret = Handler.getHandler().insertQuery(sql, para);
+        return ret;
+    }
+    
+    /**
+     * delete Priviledge by a given id
+     * @param id
+     * @return int
+     */
+    public int deletePriviledgeById(String id) {
+        String sql = "delete from Priviledge where priviledge_id = ?";
+        String[] para = {id};
+        int ret = Handler.getHandler().deleteQuery(sql, para);
+        return ret;
+    }
 }
